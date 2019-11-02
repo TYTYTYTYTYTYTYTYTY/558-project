76f11cf2ddbdaa1b695918092618066b741b0ad3
TYTYTYTYTYTYTYTYTY <sllzhlv77@gmail.com>
2019-11-01 20:24:24 -0500

    add run.sh

---
+++ run-proj.sh
@@ -0,0 +1,16 @@
+python 558-project/run_classifier.py \
+--task_name=sim \
+--do_train=true \
+--do_eval=true \
+--do_predict=true \
+--data_dir=data/ \
+--vocab_file=./wwm_uncased_L-24_H-1024_A-16/vocab.txt \
+--bert_config_file=./wwm_uncased_L-24_H-1024_A-16/bert_config.json \ 
+--init_checkpoint=./wwm_uncased_L-24_H-1024_A-16/bert_model.ckpt \ 
+--max_seq_length=400 \
+--train_batch_size=32 \
+--learning_rate=2e-5 \
+--num_train_epochs=1.0 \
+--output_dir=./bert_output/ \ 
+--do_lower_case=False \
+--save_checkpoints_steps=9999999
