e60a284d65cffa75c2a396d903fb34ecfc0a527f
TYTYTYTYTYTYTYTYTY <sllzhlv77@gmail.com>
2019-11-01 20:48:55 -0500

    2 epoch sh

--- run-proj.sh
+++ run-proj.sh
@@ -7,10 +7,10 @@ python 558-project/run_classifier.py \
 --vocab_file=./wwm_uncased_L-24_H-1024_A-16/vocab.txt \
 --bert_config_file=./wwm_uncased_L-24_H-1024_A-16/bert_config.json \ 
 --init_checkpoint=./wwm_uncased_L-24_H-1024_A-16/bert_model.ckpt \ 
---max_seq_length=400 \
+--max_seq_length=128 \
 --train_batch_size=32 \
 --learning_rate=2e-5 \
---num_train_epochs=1.0 \
+--num_train_epochs=2.0 \
 --output_dir=./bert_output/ \ 
 --do_lower_case=False \
---save_checkpoints_steps=9999999
+
