01a9490ba9484a382da557145a9230417f782852
TYTYTYTYTYTYTYTYTY <sllzhlv77@gmail.com>
2019-11-01 20:36:19 -0500

    fixed tsv

--- run_classifier.py
+++ run_classifier.py
@@ -377,7 +377,7 @@ class ColaProcessor(DataProcessor):
 class SimProcessor(DataProcessor):
     def get_train_examples(self, data_dir):
         """See base class."""
-        file_path = os.path.join(data_dir, 'train.csv')
+        file_path = os.path.join(data_dir, 'train.tsv')
         examples = []
         with open(file_path, encoding='utf-8') as f:
             reader = f.readlines()
@@ -386,14 +386,14 @@ class SimProcessor(DataProcessor):
             split_line = line.strip().split("\t")
             text_a = tokenization.convert_to_unicode(split_line [1])
             text_b = tokenization.convert_to_unicode(split_line [2])
-            label = str(split_line [0])
+            label = str(split_line [3])
             examples.append(
                 InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        file_path = os.path.join(data_dir, 'val.csv')
+        file_path = os.path.join(data_dir, 'val.tsv')
         examples = []
         with open(file_path, encoding='utf-8') as f:
             reader = f.readlines()
@@ -402,14 +402,14 @@ class SimProcessor(DataProcessor):
             split_line = line.strip().split("\t")
             text_a = tokenization.convert_to_unicode(split_line [1])
             text_b = tokenization.convert_to_unicode(split_line [2])
-            label = str(line[0])
+            label = str(line[3])
             examples.append(
                 InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
 
     def get_test_examples(self, data_dir):
         """See base class."""
-        file_path = os.path.join(data_dir, 'test.csv')
+        file_path = os.path.join(data_dir, 'test.tsv')
         examples = []
         with open(file_path, encoding='utf-8') as f:
             reader = f.readlines()
@@ -418,7 +418,7 @@ class SimProcessor(DataProcessor):
             split_line = line.strip().split("\t")
             text_a = tokenization.convert_to_unicode(split_line[1])
             text_b = tokenization.convert_to_unicode(split_line[2])
-            label = str(split_line[0])
+            label = str(split_line[3])
             examples.append(
                 InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
